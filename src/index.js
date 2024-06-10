2e81ffc1222e4c7d610940395105cf9829b51b3c
EmirYscn <87046103+EmirYscn@users.noreply.github.com>
2024-06-10 17:10:45 +0300

    add search location button

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import { config } from "./config";
+import searchSvg from "./images/search-svg.svg";
 import "./styles.css";
 
-async function fetchWeatherData(location = "istanbul") {
+// const searchBtn = document.querySelector(".search-button");
+// const svgImg = document.createElement("img");
+// svgImg.src = searchSvg;
+// searchBtn.appendChild(svgImg);
+async function fetchWeatherData(dataType = "current", location = "istanbul") {
   try {
-    const weather_api = `https://api.weatherapi.com/v1/current.json?key=${
+    const weather_api = `https://api.weatherapi.com/v1/${dataType}.json?key=${
       config.apiKey
-    }&q=${location.toLowerCase()}`;
+    }&q=${location.toLowerCase()}&days=3`;
 
     const response = await fetch(weather_api);
     const weatherData = await response.json();
-
+    console.log(weatherData);
     return weatherData;
   } catch (err) {
     console.log(err);
   }
 }
 
-function clearData(data) {
+function processData(data) {
   const weatherData = {
     name: data.location.name,
     country: data.location.country,
@@ -32,7 +37,11 @@ function clearData(data) {
   return weatherData;
 }
 
-fetchWeatherData().then((data) => {
-  const weatherData = clearData(data);
-  console.log(weatherData);
-});
+function getWeatherData(dataType, location = "istanbul") {
+  fetchWeatherData(dataType, location).then((data) => {
+    const weatherData = processData(data);
+    console.log(weatherData);
+  });
+}
+
+getWeatherData("forecast");
